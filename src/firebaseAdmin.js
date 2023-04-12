7b56d0c8b5e8497eb37246efe267e9bf5aaaa034
Mahmoud Mohamed <mahmoudmoe213@gmail.com>
2023-04-12 05:19:07 -0400

    changed the fireabase service account key to a json string instead of pointing to file path

--- src/firebaseAdmin.js
+++ src/firebaseAdmin.js
@@ -1,7 +1,7 @@
 import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
-  const serviceAccount = (process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
