a5ee4715161a7a9b57946f9f5d10c61e7d0ec1d9
Mahmoud Mohamed <mahmoudmoe213@gmail.com>
2023-04-14 11:45:59 -0400

    changed the order form styling. added icons,

--- src/pages/order.js
+++ src/pages/order.js
@@ -9,7 +9,15 @@ import { useRouter } from "next/router";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 import app from "../firebase";
 import EnableNotificationsModal from "../components/RequestNotificationModal";
-
+import {
+  IoMdLink,
+  IoMdPricetag,
+  IoMdListBox,
+  IoMdCompass,
+  IoMdImages,
+  IoMdGlobe,
+  IoMdDocument,
+} from "react-icons/io";
 
 import { stringify } from "postcss";
 import OrderAuthModal from "../components/OrderAuthModal";
@@ -39,7 +47,7 @@ function ProductForm() {
       setShowAuthModal(true);
     } else {
       const userAlreadyHasToken = await checkIfuserHasNotificationToken();
-      console.log(" Token Status: "+userAlreadyHasToken)
+      console.log(" Token Status: " + userAlreadyHasToken);
 
       if (!userAlreadyHasToken) {
         setShowNotificationModal(true);
@@ -115,7 +123,7 @@ function ProductForm() {
       const response = await fetch("/api/checkIfUserHasNotificationToken", {
         method: "GET",
       });
-  
+
       if (response.status === 200) {
         const data = await response.json();
         console.log("Token checked successfully");
@@ -129,7 +137,6 @@ function ProductForm() {
       return false;
     }
   };
-  
 
   const uploadImage = async () => {
     const imageLocation = session.user.email + "/" + uuidv4();
@@ -207,191 +214,169 @@ function ProductForm() {
   return (
     <div>
       <Header />
-
       <form
         onSubmit={handleSubmit}
-        className="max-w-lg mx-auto my-4 p-4 bg-gray-100 shadow-md rounded-md"
+        className="max-w-lg mx-auto my-4 p-4 bg-gray-100 shadow-md rounded-md space-y-4"
       >
-        <div className="mb-4">
-          <label htmlFor="url" className="block font-medium text-gray-700 mb-2">
+        <div className="flex items-center space-x-3">
+          <IoMdLink className="text-xl text-gray-600" />
+          <label htmlFor="url" className="font-medium text-gray-700">
             Product URL
           </label>
-          <input
-            type="text"
-            id="url"
-            className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter product URL"
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
-            required
-          />
         </div>
+        <input
+          type="text"
+          id="url"
+          className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Enter product URL"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          required
+        />
 
-        <div className="mb-4">
-          <label
-            htmlFor="image"
-            className="block font-medium text-gray-500 mb-1 text-lg px-2 py-1"
-          >
+        <div className="flex items-center space-x-3">
+          <IoMdImages className="text-xl text-gray-600" />
+          <label htmlFor="image" className="font-medium text-gray-700">
             Upload Image
           </label>
-          <input
-            type="file"
-            id="image"
-            // accept="image/*"
-            className="bg-white rounded-lg px-4 py-2 w-full shadow-sm border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 focus:ring-opacity-50"
-            onChange={(e) => setImage(e.target.files[0])}
-            required
-          />
         </div>
+        <input
+          type="file"
+          id="image"
+          className="bg-white rounded-lg px-4 py-2 w-full shadow-sm border border-gray-300 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 focus:ring-opacity-50"
+          onChange={(e) => setImage(e.target.files[0])}
+          required
+        />
 
-        <div className="mb-4">
-          <label
-            htmlFor="origin"
-            className="block font-medium text-gray-700 mb-2"
-          >
+        <div className="flex items-center space-x-3">
+          <IoMdGlobe className="text-xl text-gray-600" />
+          <label htmlFor="origin" className="font-medium text-gray-700">
             Origin Country
           </label>
-          <select
-            id="origin"
-            className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            value={origin}
-            onChange={(e) => setOrigin(e.target.value)}
-            required
-          >
-            <option value="">Select origin country</option>
-            {countries.map((country) => (
-              <option key={country.code} value={country.name}>
-                {country.name}
-              </option>
-            ))}
-          </select>
         </div>
-        <div className="mb-4">
-          <label
-            htmlFor="destination"
-            className="block font-medium text-gray-700 mb-2"
-          >
+        <select
+          id="origin"
+          className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          value={origin}
+          onChange={(e) => setOrigin(e.target.value)}
+          required
+        >
+          <option value="">Select origin country</option>
+          {countries.map((country) => (
+            <option key={country.code} value={country.name}>
+              {country.name}
+            </option>
+          ))}
+        </select>
+
+        <div className="flex items-center space-x-3">
+          <IoMdGlobe className="text-xl text-gray-600" />
+          <label htmlFor="destination" className="font-medium text-gray-700">
             Destination Country
           </label>
-          <select
-            id="destination"
-            className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            value={destination}
-            onChange={(e) => setDestination(e.target.value)}
-            required
-          >
-            <option value="">Select destination country</option>
-            {countries.map((country) => (
-              <option key={country.code} value={country.name}>
-                {country.name}
-              </option>
-            ))}
-          </select>
         </div>
+        <select
+          id="destination"
+          className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          value={destination}
+          onChange={(e) => setDestination(e.target.value)}
+          required
+        >
+          <option value="">Select destination country</option>
+          {countries.map((country) => (
+            <option key={country.code} value={country.name}>
+              {country.name}
+            </option>
+          ))}
+        </select>
 
-        <div className="mb-4">
-          <label
-            htmlFor="category"
-            className="block font-medium text-gray-700 mb-2"
-          >
+        <div className="flex items-center space-x-3">
+          <IoMdListBox className="text-xl text-gray-600" />
+          <label htmlFor="category" className="font-medium text-gray-700">
             Category
           </label>
-          <select
-            id="category"
-            className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-            required
-          >
-            <option value="">Select a category</option>
-            <option value="clothing">Clothing</option>
-            <option value="beauty">Beauty and personal care</option>
-
-            <option value="clothing">Electronics</option>
-            <option value="beauty">Home and garden</option>
-
-            <option value="clothing">Clothing</option>
-            <option value="beauty">Sports and outdoors</option>
-
-            <option value="clothing">Toys and games</option>
-            <option value="beauty">Beauty</option>
-
-            <option value="clothing">Health and wellness</option>
-            <option value="beauty">Automotive</option>
-            {/* Add more options for other categories */}
-          </select>
         </div>
+        <select
+          id="category"
+          className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          required
+        >
+          <option value="">Select a category</option>
+          <option value="clothing">Clothing</option>
+          <option value="beauty">Beauty and personal care</option>
+          <option value="electronics">Electronics</option>
+          <option value="home">Home and garden</option>
+          <option value="sports">Sports and outdoors</option>
+          <option value="toys">Toys and games</option>
+          <option value="health">Health and wellness</option>
+          <option value="automotive">Automotive</option>
+          {/* Add more options for other categories */}
+        </select>
 
-        <div className="mb-4">
-          <label
-            htmlFor="description"
-            className="block font-medium text-gray-700 mb-2"
-          >
+        <div className="flex items-center space-x-3">
+          <IoMdDocument className="text-xl text-gray-600" />
+          <label htmlFor="description" className="font-medium text-gray-700">
             Description
           </label>
-          <textarea
-            id="description"
-            className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter product description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            required
-          />
         </div>
+        <textarea
+          id="description"
+          className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Enter product description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        />
 
-        <div className="mb-4">
-          <label
-            htmlFor="title"
-            className="block font-medium text-gray-700 mb-2"
-          >
+        <div className="flex items-center space-x-3">
+          <IoMdListBox className="text-xl text-gray-600" />
+          <label htmlFor="title" className="font-medium text-gray-700">
             Title
           </label>
-          <input
-            type="text"
-            id="title"
-            className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter product title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            required
-          />
         </div>
+        <input
+          type="text"
+          id="title"
+          className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Enter product title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
 
-        <div className="mb-4">
-          <label
-            htmlFor="price"
-            className="block font-medium text-gray-700 mb-2"
-          >
+        <div className="flex items-center space-x-3">
+          <IoMdPricetag className="text-xl text-gray-600" />
+          <label htmlFor="price" className="font-medium text-gray-700">
             Price
           </label>
-          <input
-            type="number"
-            id="price"
-            className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter product price"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-            required
-          />
         </div>
+        <input
+          type="number"
+          id="price"
+          className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Enter product price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          required
+        />
 
-        <div className="mb-4">
-          <label
-            htmlFor="pickupLocation"
-            className="block font-medium text-gray-700 mb-2"
-          >
+        <div className="flex items-center space-x-3">
+          <IoMdCompass className="text-xl text-gray-600" />
+          <label htmlFor="pickupLocation" className="font-medium text-gray-700">
             Pickup Location
           </label>
-          <input
-            type="text"
-            id="pickupLocation"
-            className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Enter Pickup Location"
-            value={pickupLocation}
-            onChange={(e) => setPickupLocation(e.target.value)}
-            required
-          />
         </div>
+        <input
+          type="text"
+          id="pickupLocation"
+          className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Enter Pickup Location"
+          value={pickupLocation}
+          onChange={(e) => setPickupLocation(e.target.value)}
+          required
+        />
 
         <div className="mt-8">
           <button
