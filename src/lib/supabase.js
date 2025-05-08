6d7a5ab509d1e558c8cba4316412522a5e74fec6
Mahmoudmoe123 <mahmoudmoe213@gmail.com>
2025-05-08 01:17:22 -0400

    fixed supabase.js client initalization in vercel

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,8 +3,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  "https://ffyuiicnuiyyufohhsca.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmeXVpaWNudWl5eXVmb2hoc2NhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgyMDU4NzcsImV4cCI6MTk5Mzc4MTg3N30.dBMmhouMgYab22Ar3NWJ1NvfUmswHhbI_o3LnJeuifI"
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
 export default supabase;
