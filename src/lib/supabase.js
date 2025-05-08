34b470ce0838b0dab5e5d14efae4d9bab965bc05
Mahmoudmoe123 <mahmoudmoe213@gmail.com>
2025-05-08 02:47:22 -0400

    I think this was the last working commit

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,10 +1,8 @@
-// lib/supabase.js
-
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  "https://nrnbofdrdyspkcjtyaus.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ybmJvZmRyZHlzcGtjanR5YXVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2MTE1MzYsImV4cCI6MjA2MjE4NzUzNn0.L23sEn13jlhLPBMD-WlzlLTZBFCkBrR3JFTwnyc6JSQ"
+  "https://ffyuiicnuiyyufohhsca.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZmeXVpaWNudWl5eXVmb2hoc2NhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgyMDU4NzcsImV4cCI6MTk5Mzc4MTg3N30.dBMmhouMgYab22Ar3NWJ1NvfUmswHhbI_o3LnJeuifI"
 );
 
 export default supabase;
