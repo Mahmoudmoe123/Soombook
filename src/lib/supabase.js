07854eabe32dc007fe2572a04743eca2ba254a36
Mahmoudmoe123 <mahmoudmoe213@gmail.com>
2025-05-08 02:19:45 -0400

    hardcoded new supabase url and supbase anon key in supabase.js file

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,8 +3,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  "https://nrnbofdrdyspkcjtyaus.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ybmJvZmRyZHlzcGtjanR5YXVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2MTE1MzYsImV4cCI6MjA2MjE4NzUzNn0.L23sEn13jlhLPBMD-WlzlLTZBFCkBrR3JFTwnyc6JSQ"
 );
 
 export default supabase;
