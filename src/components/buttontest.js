de7a2695caf53452e9220f709d03141dde2b2637
Mahmoudmoe123 <mahmoudmoe213@gmail.com>
2025-06-07 23:55:05 -0400

    (feat): added email input field

--- src/components/buttontest.js
+++ src/components/buttontest.js
@@ -2,10 +2,11 @@ import { useState } from "react";
 
 function MyForm() {
   const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(`The name you entered was: ${name}`);
+    alert(`Name: ${name}\nEmail: ${email}`);
   }
 
   return (
@@ -15,6 +16,14 @@ function MyForm() {
           type="text" 
           value={name}
           onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>Enter your email:
+        <input 
+          type="email" 
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
       </label>
       <input type="submit" />
